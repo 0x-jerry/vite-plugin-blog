2be31541cc999d5c4cf3b81e02e3316afc8fa6e5
0x-jerry <x.jerry.wang@gmail.com>
2021-10-08 10:17:16 +0800

    chore: update example

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
+      '~blog/': `${path.resolve(__dirname, '.blog')}/`,
     },
   },
   plugins: [
